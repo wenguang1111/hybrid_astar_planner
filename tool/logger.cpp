d82a975160c745ba8cd2e808855038106c9c69d9
Wenguang XU <wenguang.xu@thi.de>
2024-01-22 17:19:29 +0100

    logger works with diiferent type of data

--- tool/logger.cpp
+++ tool/logger.cpp
@@ -3,6 +3,17 @@
 #include <map>
 #include <vector>
 
+ class BaseMap {
+    public:
+        virtual ~BaseMap() {}
+    };
+
+template<typename T>
+class TypedMap : public BaseMap {
+public:
+    std::map<std::string, std::vector<T>> data;
+};
+
 class Logger
 {
 public:
@@ -19,61 +30,69 @@ public:
     template<typename T>
     void saveData(std::string variable, T value)
     {
-        std::map<std::string,std::vector<T>>* data;
+        BaseMap* base = nullptr;
 
         if(std::is_same<T, int>::value)
         {
-            data = &data_i;
+            base = &data_i;
         }
         else if (std::is_same<T, float>::value)
         {
-            data = &data_f;
+            base = &data_f;
         }
         else if (std::is_same<T, double>::value)
         {
-            data = &data_d;
+            base = &data_d;
         }
         else{
             std::cout << "logger.cpp: undefined datatype"<< std::endl;
         }
 
-        typename std::map<std::string,std::vector<T>>::iterator it=data->find(variable);
+        typename std::map<std::string, std::vector<T>>::iterator it=
+            static_cast<TypedMap<T>*>(base)->data.find(variable);
 
-        if(it != data->end())
+        if(it != static_cast<TypedMap<T>*>(base)->data.end())
             it->second.push_back(value);
         else       
-            data->insert(std::pair<std::string, std::vector<T>>(variable,std::vector<T>{value}.at(0)));
+            static_cast<TypedMap<T>*>(base)->data.insert(
+                std::pair<std::string, std::vector<T>>(variable,std::vector<T>{value}));
     }
 
-    // template<typename T>
-    // std::string getData(std::string variable, int n)
-    // {
-    //     std::string ans="";
-    //     std::map<std::string,std::vector<T>>* data;
+    template<typename T>
+    std::string getData(std::string variable, int n)
+    {
+        std::string ans="";
+        BaseMap* base = nullptr;
 
-    //     if(std::is_same<T, int>::value)
-    //     {
-    //         data = &data_i;
-    //     }
-    //     else if (std::is_same<T, float>::value)
-    //     {
-    //         data = &data_f;
-    //     }
-    //     else if (std::is_same<T, double>::value)
-    //     {
-    //         data = &data_d;
-    //     }
-    //     else{
-    //         std::cout << "logger.cpp: undefined datatype"<< std::endl;
-    //     }
+        if(std::is_same<T, int>::value)
+        {
+            base = &data_i;
+        }
+        else if (std::is_same<T, float>::value)
+        {
+            base = &data_f;
+        }
+        else if (std::is_same<T, double>::value)
+        {
+            base = &data_d;
+        }
+        else{
+            std::cout << "logger.cpp: undefined datatype"<< std::endl;
+        }
         
-    //     typename std::map<std::string,std::vector<T>>::iterator it=data->find(variable);
-    //     if(it != data->end())
-    //         ans = std::to_string(data[variable].at(n));
-    //     return ans;
-    // }
+        typename std::map<std::string,std::vector<T>>::iterator it=static_cast<TypedMap<T>*>(base)->data.find(variable);
+        if(it != static_cast<TypedMap<T>*>(base)->data.end())
+        {
+            if(it->second.size()>n)
+                ans = std::to_string(it->second.at(n));
+            else
+                std::cerr << "[ERROR] [logger.cpp] out of range" << std::endl;
+        }
+            
+        return ans;
+    }
     
-    //cant write data to csv, why?
+    //cant write data to csv, why? 
     void writeDataToCSV()
    {
     fout.open("variableData.csv", std::ios::out | std::ios::app); 
@@ -84,9 +103,9 @@ private:
     Logger() = default;
     static Logger* log;
     std::fstream fout;
-    std::map<std::string,std::vector<int>> data_i;
-    std::map<std::string,std::vector<float>> data_f;
-    std::map<std::string,std::vector<double>> data_d;
+    TypedMap<int> data_i;
+    TypedMap<float> data_f;
+    TypedMap<double> data_d;
 };
 
 Logger* Logger::log = nullptr;
@@ -98,12 +117,12 @@ int main()
     int i =3;
     double d=1.234;
     float f=2.34;
-    myLogger->saveData<int>("varaible_1", i);
-    // myLogger->saveData<double>("v_2", d);
-    // myLogger->saveData<float>("anyT", f);
+    myLogger->saveData<int>("variable_1", i);
+    myLogger->saveData<double>("v_2", d);
+    myLogger->saveData<float>("anyT", f);
     myLogger->saveData<int>("variable_1", 505);
 
-    // std::cout << myLogger->getData<int>("variable_1",0);
-    // std::cout << myLogger->getData<int>("variable_1",1);
-    // std::cout << myLogger->getData<int>("variable_1",2);
+    std::cout << myLogger->getData<int>("variable_1",0)<< std::endl;
+    std::cout << myLogger->getData<int>("variable_1",1)<< std::endl;
+    std::cout << myLogger->getData<double>("variable_1",2)<< std::endl;
 }
