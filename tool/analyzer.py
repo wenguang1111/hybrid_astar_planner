9562766be1008fd82999d337c9de740f533bc380
Wenguang XU <wenguang.xu@thi.de>
2024-01-30 11:34:41 +0100

    update analyzer

--- tool/analyzer.py
+++ tool/analyzer.py
@@ -10,16 +10,19 @@ for column in data.columns:
     max_value = data[column].max()
     min_value = data[column].min()
     std_dev = data[column].std()
-    num_cal = len(data[column])
+    num_cal = data[column].count()
     column_stats[column] = {'max_value': max_value, 'min_value': min_value, 'std_dev': std_dev, 'num_cal': num_cal}
 
+# Find the maximum number of rows among all columns
+max_num_cal = max(stats['num_cal'] for stats in column_stats.values())
 
 for column, stats in column_stats.items():
     print(f"variable： {column}:")
     print("max_value:", stats['max_value'])
     print("min_value:", stats['min_value'])
     print("std_dev:", stats['std_dev'])
-    print("num_cal", stats['num_cal'])
+    print("num_cal:", stats['num_cal'])
+    print("num_cal/max(num_call[i]):", stats['num_cal']/max_num_cal*100)
     print("\n")
 
 # values = data['Car::getOutline()::head_l[i]']
